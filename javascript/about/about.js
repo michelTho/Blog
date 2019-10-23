3cee115c2322f73bed4f9327fc707d352adbde0c
michelTho <michel.thomazo@polytechnique.edu>
2019-10-23 10:25:09 +0200

    remove unused resources

--- javascript/about/about.js
+++
@@ -1,3 +0,0 @@
-export function main(){
-    console.log('about.main loaded');
-}
--- javascript/firstGame/firstGameConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    NUMBER_ENEMIES: 10,
-    HEIGHT: 600,
-    WIDTH: 800,
-    ENEMY_RADIUS: 5,
-    HERO_RADIUS: 8,
-};
-
-export default config;
