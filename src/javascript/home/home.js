8aa59f31293ece8e9ffa60a086191b840968c73e
michelTho <michel.thomazo@polytechnique.edu>
2019-10-23 10:24:32 +0200

    add all source file in src

---
+++ src/javascript/home/home.js
@@ -0,0 +1,3 @@
+export function main(){
+    console.log('home.main loaded');   
+}
---
+++ src/javascript/main.js
@@ -0,0 +1,22 @@
+var container = document.getElementById('container');
+
+function loadJavascript(name) {
+    import (`./${name}/${name}.js`).then((module) => {module.main()});
+}
+
+function route() {
+    let hash = window.location.hash;
+    hash = hash ? hash : '#home';
+    name = hash.slice(1, hash.length);
+    let url = `../templates/${name}.html`;
+    fetch(url)
+        .then((response) => { return response.text() })
+        .then((text) => { 
+            container.innerHTML = text;
+            loadJavascript(name);
+        });
+}
+
+window.onhashchange = route;
+route();
+
