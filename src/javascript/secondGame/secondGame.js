52419fc77a3f55f885381a6a9216509355e3757f
michelTho <michel.thomazo@polytechnique.edu>
2019-10-24 17:32:57 +0200

    add the second game to the structure

--- src/javascript/secondGame/secondGame.js
+++ src/javascript/secondGame/secondGame.js
@@ -1,5 +1,36 @@
 'use strict';
 
-export function main(){
-    console.log('secondGame.main loaded');   
+import config from './secondGameConfig.js';
+
+var scene, camera, renderer;
+var geometry, material, cube;
+
+var article;
+
+function loadCanvas() {
+    article = document.getElementById('container').firstChild;
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, config.WIDTH / config.HEIGHT, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(config.WIDTH, config.HEIGHT);
+    article.appendChild(renderer.domElement);
+
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+export function main() {
+    loadCanvas();
+    animate();
 }
---
+++ src/javascript/secondGame/secondGameConfig.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const config = {
+    HEIGHT: 600,
+    WIDTH: 800,
+};
+
+export default config;
