6621671a089be6cf8a7dbd1143cbaf5644718817
michelTho <michel.thomazo@polytechnique.edu>
2019-10-24 17:37:23 +0200

    first animation

--- src/javascript/secondGame/secondGame.js
+++ src/javascript/secondGame/secondGame.js
@@ -7,9 +7,11 @@ var geometry, material, cube;
 
 var article;
 
-function loadCanvas() {
+function loadDocumentElement() {
     article = document.getElementById('container').firstChild;
+}
 
+function load3DMainObjects() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, config.WIDTH / config.HEIGHT, 0.1, 1000);
 
@@ -18,19 +20,22 @@ function loadCanvas() {
     article.appendChild(renderer.domElement);
 
     geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    camera.position.z = 5;
+    camera.position.z = 3;
 }
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 export function main() {
-    loadCanvas();
+    loadDocumentElement();
+    load3DMainObjects();
     animate();
 }
